5ea0d0cc2ce2b7cfd9b1417f2ecf9231f2b29c50
Billy Bunter <billy@bunter.com>
2025-02-19 19:58:40 +0000

    First

---
+++ Components/BOEDemoMessageLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BOEDemoMessageLibrary
+{
+    public class MessagePrinter
+    {
+        public string? Message { get; set; }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Your message is as follows....: {0}", Message);
+        }
+    }
+}
---
+++ ShellRunner/ShellRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BOEDemoMessageLibrary;
+
+namespace ShellRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessagePrinter mp = new MessagePrinter();
+            mp.Message = "Hello Bob";
+            mp.DisplayMessage();     
+        }
+    }
+}
